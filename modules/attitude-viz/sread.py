40e02153cf3edd9a520c54352e47d6f83063cf63
Nathaniel Mallick <nmm109@pitt.edu>
2023-10-16 22:16:30 -0400

    added signaling sync for serial rad

--- modules/attitude-viz/sread.py
+++ modules/attitude-viz/sread.py
@@ -1,15 +1,16 @@
-
 import struct
 import serial
 import threading
+import subprocess
 
 from qmath import EulerAngles
 
 class DeviceInterface:
     def __init__(self, port="/dev/ttyUSB0", baud=115200):
         __port__ = port
-        self.__dev__ = serial.Serial(__port__, baud, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
-        self.__dev__.reset_input_buffer()
+        self.__dev__ = serial.Serial(__port__, baud, timeout=0.5)
+        self.__dev__.close()
+        self.__dev__.open()
 
         self.__NUM_FLOATS__ = 4
         self.__FLOAT_SIZE__ = 4
@@ -20,12 +21,20 @@ class DeviceInterface:
         self.angles = EulerAngles()
 
         self.buf = None
+        self.trigger = bytearray([1])
         self.quaternion = []
         self.done = False
 
     def readData(self):
+        self.__dev__.reset_input_buffer()
         while not self.done:
+            self.__dev__.write(self.trigger)
             self.buf = self.__dev__.read(self.__TOTAL_BYTES__)
+
+            if len(self.buf) != self.__TOTAL_BYTES__:
+                print("timedout")
+                continue
+
             self.quaternion.clear()
             for i in range(self.__NUM_FLOATS__):
                 start = self.__NUM_FLOATS__*i
@@ -47,6 +56,9 @@ class DeviceInterface:
         self.__thread__.join()
 
 if __name__ == '__main__':
+
+    subprocess.run(["st-flash", "reset"])
+
     dev = DeviceInterface(port="/dev/ttyACM0")
     try:
         dev.start()
