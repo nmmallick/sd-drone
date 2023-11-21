65569a5a3029cbaef5991f93a69f25c46da8b995
NMMallick <nmm109@pitt.edu>
2023-11-21 16:04:21 -0500

    added takeoff and land test

---
+++ modules/offboard/tests/takeoff_land.cc
@@ -0,0 +1,22 @@
+#include <chrono>
+#include <iostream>
+
+#include <DroneInterface.hh>
+
+int main(int argc, char **argv)
+{
+    auto drone = offboard::DroneInterface();
+    drone.connect("udp://:14540");
+
+    // Arm and takeoff
+    if (!drone.start())
+	throw std::runtime_error("Could not take arm and take off.");
+
+    std::cout << "In air\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    std::cout << "Landing drone\n";
+    drone.stop();
+
+    return 0;
+}
